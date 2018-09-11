76d030439ab59c49d33d15853bb6c0d67cb31187
dzrcool <dongzongran@qq.com>
2018-09-11 16:03:09 +0800

    Add piece object
    Update some code

--- Ex_1/draw.py
+++ Ex_1/draw.py
@@ -13,18 +13,30 @@ class Board():
     def draw(self):
         pygame.draw.rect(self.screen, self.color, (self.left, self.top, self.width, self.height))
  
-    	
-
+class Piece():
+    def __init__(self, points, color, screen):
+        self.points = points
+        self.color = color
+        self.screen = screen
+        
+    def draw(self):
+        pygame.draw.lines(self.screen, self.color, True, self.points, 1)
+    
 def run():
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Cutting and Packing")   
-    WHITE = (255,255,255)
+    WHITE = (255, 255, 255)
     screen.fill(WHITE)
     
-    BLUE = (0,0,255)
+    BLUE = (0, 0, 255)
     b = Board(100, 100, 1000, 600, BLUE, screen)
     b.draw()
+    
+    RED = (255, 0, 0)
+    points = [(100, 100), (120, 200), (130, 240), (150, 300), (120, 130)]
+    p1 = Piece(points, RED, screen)
+    p1.draw()
     
     while True:
         for event in pygame.event.get():
